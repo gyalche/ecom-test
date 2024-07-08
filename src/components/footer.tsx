0ef74cb58b3958f9c1f830fcf04919bf916c5c2d
Dawa Sherpa <fupulamu01@gmail.com>
2024-07-08 18:36:57 +0545

    footer completed

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,10 +1,46 @@
 import React from "react";
 import Wrapper from "./Wrapper";
+import { fotterSocialMedia, navbarData } from "@/constants";
+import Link from "next/link";
+import footerLogo from "../assets/footerLogo.png";
+import Image from "next/image";
 
 type Props = {};
 
 const Footer = (props: Props) => {
-  return <Wrapper>Footer</Wrapper>;
+  return (
+    <Wrapper>
+      <div className="footer flex justify-around text-white">
+        {navbarData?.map((data, index) => (
+          <div key={data?.title} className="flex">
+            <div className="flex flex-col items-start  gap-7 text-white">
+              <p className="text-white">{data?.title}</p>
+              <div className="flex items-start flex-col">
+                {data?.subTitle?.map((val, index) => (
+                  <Link key={index} href={val.url} className="link_word">
+                    {val?.label}
+                  </Link>
+                ))}
+              </div>
+            </div>
+          </div>
+        ))}
+        <div className="flex flex-col gap-2">
+          <Image src={footerLogo} alt="footerlogo" />
+          <div className="link_word flex items-start justify-start flex-col gap-1">
+            <p className="link_word">60-49 Road 11378 New York</p>
+            <a href="tel:65 11 188 888">+65 11 188 888</a>
+            <a href="mailto:chicseduire@gmail.com">chicseduire@gmail.com</a>
+          </div>
+          <div className="flex gap-4">
+            {fotterSocialMedia?.map(({ icon, alt }) => (
+              <Image key={alt} src={icon} alt={alt} />
+            ))}
+          </div>
+        </div>
+      </div>
+    </Wrapper>
+  );
 };
 
 export default Footer;
--- src/constants/index.ts
+++ src/constants/index.ts
@@ -2,6 +2,10 @@ import cat1 from "../assets/cat1.png";
 import cat2 from "../assets/cat2.png";
 import cat3 from "../assets/cat3.png";
 import cat4 from "../assets/cat4.png";
+import facebook from "../assets/facebook.png";
+import instagram from "../assets/instagram.png";
+import whatsapp from "../assets/whatsapp.png";
+import twitter from "../assets/twitter.png";
 export const navItems = [
   {
     label: "Winter",
@@ -85,5 +89,97 @@ export const moreCategory = [
   {
     image: cat4,
     label: "cat4",
+  },
+];
+
+export const navbarData = [
+  {
+    title: "Navigation",
+    subTitle: [
+      {
+        label: "Home",
+        url: "/",
+      },
+      {
+        label: "About",
+        url: "/about",
+      },
+      {
+        label: "Service",
+        url: "/service",
+      },
+      {
+        label: "Terms & Condition",
+        url: "/",
+      },
+      {
+        label: "Contact",
+        url: "/contact",
+      },
+    ],
+  },
+  {
+    title: "Shop",
+    subTitle: [
+      {
+        label: "Women",
+        url: "/",
+      },
+      {
+        label: "Men",
+        url: "/",
+      },
+      {
+        label: "Jacket",
+        url: "/",
+      },
+      {
+        label: "New Arrivals",
+        url: "/",
+      },
+      {
+        label: "Hot Picks",
+        url: "/",
+      },
+    ],
+  },
+  {
+    title: "Explore",
+    subTitle: [
+      {
+        label: "T-Shirts",
+        url: "/",
+      },
+      {
+        label: "My Orders",
+        url: "/",
+      },
+      {
+        label: "Widhlist",
+        url: "/",
+      },
+      {
+        label: "Coarts",
+        url: "/",
+      },
+    ],
+  },
+];
+export const fotterSocialMedia = [
+  {
+    icon: facebook,
+    alt: "facebook",
+  },
+  {
+    icon: instagram,
+    alt: "instagram",
+  },
+  {
+    icon: whatsapp,
+    alt: "whatsapp",
+  },
+  {
+    icon: twitter,
+    alt: "twitter",
   },
 ];
